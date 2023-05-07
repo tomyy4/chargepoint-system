11e49fa1e962b35f646ab68fc2ea0b05a4e1ecdd
tomeullompartpiza <t.llompart@avoristravel.com>
2023-05-07 12:34:21 +0200

    swagger

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path
+from rest_framework import permissions
 
 from api.views import ChargePointList, ChargePointDetail, ChargePointDelete, ChargePointCreate, ChargePointEdit
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny,],
+)
+
+
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('chargepoint/', ChargePointList.as_view(), name='charge_point_list'),
     path('chargepoint/create/', ChargePointCreate.as_view(), name='charge_point_create'),
     path('chargepoint/<int:pk>/update/', ChargePointEdit.as_view(), name='charge_point_edit'),
--- src/main/settings.py
+++ src/main/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
 
     'main',
     'core',
